7e48c72d84878a92043238cc4f73d6f0a0b482d8
Dinis Costa <scorpaust@hotmail.com>
2021-01-24 14:36:25 +0000

    Created pawn class and its components.

---
+++ Source/ToonTanks/Pawns/PawnBase.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SceneComponent.h"
+#include "PawnBase.generated.h"
+
+UCLASS()
+class TOONTANKS_API APawnBase : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APawnBase();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+
+	class UCapsuleComponent* CapsuleComponent;
+
+	class UStaticMeshComponent* BaseMesh;
+
+	class UStaticMeshComponent* TurretMesh;
+
+	class USceneComponent* ProjectileSpawnPoint;
+
+};
